b41f1088ecd324dd06ba83d46685367cd30d513b
Joakim Soderlund <joakim.soderlund@gmail.com>
2025-06-07 19:10:28 +0200

    Add host for Furver to ingress

--- ingress/src/main.rs
+++ ingress/src/main.rs
@@ -1,11 +1,18 @@
 use reversi::Host;
 use reversi::Reversi;
 
-const HOSTS: &[Host] = &[Host {
-    domain: "www.fimfarchive.net",
-    redirects: &["fimfarchive.net"],
-    upstream: "localhost:34407",
-}];
+const HOSTS: &[Host] = &[
+    Host {
+        domain: "www.fimfarchive.net",
+        redirects: &["fimfarchive.net"],
+        upstream: "localhost:34407",
+    },
+    Host {
+        domain: "furver.se",
+        redirects: &["www.furver.se"],
+        upstream: "localhost:49211",
+    },
+];
 
 pub fn main() -> ! {
     Reversi::from(HOSTS).run()
